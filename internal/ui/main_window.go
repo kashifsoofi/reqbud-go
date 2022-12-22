12a5fe5d15a8beb5e4987d714760d369605b6114
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-12-22 18:27:43 +0000

    Update main window

--- internal/ui/main_window.go
+++ internal/ui/main_window.go
@@ -11,6 +11,31 @@ var requests = []string{}
 func NewMainWindow(a fyne.App) fyne.Window {
 	w := a.NewWindow("Request Buddy")
 
+	leftNavigation := createNavigation()
+
+	method := widget.NewSelect([]string{"HEAD", "GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"}, func(s string) {})
+	method.Selected = "GET"
+	url := widget.NewEntry()
+	send := widget.NewButton("Send", func() {})
+	addressBar := container.NewBorder(nil, nil, method, send, url)
+
+	content := container.NewAppTabs(
+		container.NewTabItem("Request", widget.NewLabel("Requst content")),
+		container.NewTabItem("Response", widget.NewLabel("Response content")),
+	)
+
+	mainContent := container.NewBorder(
+		addressBar, nil, nil, nil, content)
+
+	split := container.NewHSplit(leftNavigation, mainContent)
+	split.Offset = 0.2
+
+	w.SetContent(split)
+	w.Resize(fyne.NewSize(800, 600))
+	return w
+}
+
+func createNavigation() fyne.CanvasObject {
 	requestList := widget.NewList(
 		func() int { return len(requests) },
 		func() fyne.CanvasObject {
@@ -19,7 +44,8 @@ func NewMainWindow(a fyne.App) fyne.Window {
 		func(i widget.ListItemID, o fyne.CanvasObject) {
 			o.(*widget.Label).SetText(requests[i])
 		})
-	requestNavigation := container.NewBorder(
+
+	return container.NewBorder(
 		widget.NewButton("New Request", func() {
 			requests = append(requests, "New Request")
 			requestList.Refresh()
@@ -29,17 +55,4 @@ func NewMainWindow(a fyne.App) fyne.Window {
 		nil,
 		requestList,
 	)
-
-	requestContent := widget.NewMultiLineEntry()
-	requestContent.SetText("Request content go here")
-	responseContent := widget.NewLabel("Response content go here")
-
-	mainContent := container.NewBorder(
-		nil, nil, nil, nil, container.NewMax(container.NewVSplit(requestContent, responseContent)))
-
-	split := container.NewHSplit(requestNavigation, mainContent)
-
-	w.SetContent(split)
-	w.Resize(fyne.NewSize(640, 460))
-	return w
 }
