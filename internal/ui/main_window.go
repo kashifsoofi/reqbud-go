728f21c6c381aadf8d18607711ae231d8c4bb3fa
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-01-23 23:19:47 +0000

    Make a get request on Send button and display response text in widget

--- internal/ui/main_window.go
+++ internal/ui/main_window.go
@@ -1,6 +1,10 @@
 package ui
 
 import (
+	"io"
+	"net/http"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -13,16 +17,21 @@ func NewMainWindow(a fyne.App) fyne.Window {
 
 	leftNavigation := createNavigation()
 
+	responseContent := widget.NewLabel("Response content")
+	content := container.NewAppTabs(
+		container.NewTabItem("Request", widget.NewLabel("Requst content")),
+		container.NewTabItem("Response", responseContent),
+	)
+
 	method := widget.NewSelect([]string{"HEAD", "GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"}, func(s string) {})
 	method.Selected = "GET"
 	url := widget.NewEntry()
-	send := widget.NewButton("Send", func() {})
+	send := widget.NewButton("Send", func() {
+		responseText := makeRequest(method.Selected, url.Text, "")
+		responseContent.SetText(responseText)
+		responseContent.Refresh()
+	})
 	addressBar := container.NewBorder(nil, nil, method, send, url)
-
-	content := container.NewAppTabs(
-		container.NewTabItem("Request", widget.NewLabel("Requst content")),
-		container.NewTabItem("Response", widget.NewLabel("Response content")),
-	)
 
 	mainContent := container.NewBorder(
 		addressBar, nil, nil, nil, content)
@@ -55,4 +64,39 @@ func createNavigation() fyne.CanvasObject {
 		nil,
 		requestList,
 	)
+}
+
+func createRequest(method, url, data string) (*http.Request, error) {
+	var body io.Reader
+	if len(data) > 0 {
+		body = strings.NewReader(data)
+	}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func makeRequest(method, url, data string) string {
+	req, err := createRequest(method, url, data)
+	if err != nil {
+		return "Error creating request"
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "Error making request"
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "Error reading response body"
+	}
+
+	return string(body)
 }
